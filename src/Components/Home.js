a6c705fc5413f1eca5d1ffea3ee0974c4e89b576
Sampreeth2002 <sampreeth.2002@gmail.com>
2023-05-03 12:20:21 +0530

    added home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,6 @@
 import React, { useState, useContext } from "react";
 import { ItemsContext } from "./Context/ItemsContext";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import RadioButtonCheckedIcon from "@mui/icons-material/RadioButtonChecked";
-import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
-import VideocamIcon from "@mui/icons-material/Videocam";
-import VideocamOffIcon from "@mui/icons-material/VideocamOff";
-import MicIcon from "@mui/icons-material/Mic";
-import MicOffIcon from "@mui/icons-material/MicOff";
+import Button from "@mui/material/Button";
 
 import "../App.css";
 import "./Room.css";
@@ -39,144 +32,193 @@ function Home() {
   return (
     <>
       <div>
-        <div
-          style={{
-            paddingTop: "4vh",
-            paddingBottom: "2vh",
-            fontSize: "2vw",
-            color: "white",
-            textAlign: "center",
-          }}
-        >
-          Live Streams
+        <div style={{ textAlign: "center", color: "white" }}>
+          <br />
+          <br />
+          <h1 style={{ color: "hsl(178, 100%, 50%)" }}>VidCast</h1>
+          <p style={{ color: "hsl(215, 51%, 70%)" }}>
+            Where NFTs Ignite Creativity and Foster Community
+          </p>
         </div>
-        <div className="card-container">
-          <div style={{ padding: "2vw" }}>
-            <figure className="snip1527">
-              <div className="image">
-                <img alt="pr-sample23" />
-              </div>
-              <figcaption>
-                {/* <div className="date">
-                  <span className="day">28</span>
-                  <span className="month">Oct</span>
-                </div> */}
-                <h3>Sampreeth Live Stream</h3>
-                <p>
-                  You know what we need, Hobbes? We need an attitude. Yeah, you
-                  can't be cool if you don't have an attitude.
-                </p>
-              </figcaption>
-              <a href="/smapreeth"></a>
-            </figure>
-          </div>
-        </div>
-      </div>
+        <br />
+        <br />
 
-      <div>
-        <div
+        {/* <div
           style={{
-            paddingBottom: "2vw",
-            fontSize: "2vw",
-            color: "white",
             textAlign: "center",
+            color: "white",
+            width: "1100px",
+            display: "flex",
+            justifyContent: "center",
+            margin: "0 auto",
+            display: "none",
           }}
         >
-          Recordings
-        </div>
-        <div className="card-container">
-          <div style={{ padding: "2vw" }}>
-            <figure className="snip1527">
-              <div className="image">
-                <img
-                  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/pr-sample23.jpg"
-                  alt="pr-sample23"
-                />
-              </div>
-              <figcaption>
-                <div className="date">
-                  <span className="day">28</span>
-                  <span className="month">Oct</span>
-                </div>
-                <h3>The World Ended Yesterday</h3>
-                <p>
-                  You know what we need, Hobbes? We need an attitude. Yeah, you
-                  can't be cool if you don't have an attitude.
-                </p>
-              </figcaption>
-              <a href="#"></a>
-            </figure>
+          <p style={{ marginRight: "20px" }}>
+            Welcome to VidCast, the ultimate platform for users to become both
+            creators and viewers. Craft your unique NFT collection and list it
+            on our marketplace, featuring both Standard Edition and Special
+            Edition NFTs. Users can purchase their favorite creator's NFTs and
+            enjoy additional perks when buying Special Edition NFTs.
+          </p>
+          <br />
+          <br />
+          <p style={{ marginRight: "20px" }}>
+            Easily manage and view your NFT purchases on VidCast. Creators can
+            set up exclusive rooms for their NFT buyers, powered by Huddle SDK,
+            where Special Edition NFT holders can act as co-hosts and engage
+            using their camera and microphone. Standard NFT holders can watch
+            and listen to the live video of co-hosts and the creator.
+          </p>
+          <br />
+          <br />
+          <p style={{ marginRight: "20px" }}>
+            Special Edition NFT holders gain access to a separate chat section
+            for direct communication with creators. VidCast utilizes Lighthouse
+            SDK for secure storage of NFT files, such as PNG and JPEG images,
+            and deploys smart contracts on the FVM using Filecoin Hyperspace
+            (testnet) for a robust NFT ecosystem.
+          </p>
+        </div> */}
+
+        <div style={{ color: "white", marginLeft: "20px" }}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <div style={{ marginRight: "100px" }}>
+              <p
+                style={{
+                  marginLeft: "20px",
+                  width: "600px",
+                  textAlign: "center",
+                }}
+              >
+                {" "}
+                VidCast is a unique platform that allows creators to monetize
+                their content through NFT collections, while viewers can have
+                access to exclusive perks and features based on the NFTs they
+                purchase. The platform also provides a community aspect, where
+                users can interact with each other and the creators in real-time
+                through the room and chat features.
+              </p>
+            </div>
+            <div>
+              <img
+                src="https://www.tubefilter.com/wp-content/uploads/2021/05/bright-video-platform-1400x825.jpg"
+                alt=""
+                width={"500px"}
+              />
+            </div>
+          </div>
+          <br />
+          <br />
+          <div style={{ textAlign: "center" }}>
+            <h4 style={{ color: "hsl(215, 51%, 70%)" }}>
+              Craft and Showcase Your Unique NFT Collection
+            </h4>
+            <p style={{ marginTop: "15px" }}>
+              {" "}
+              You can list your NFTs in our marketplace, which features both
+              Standard and Special Editions. We use LightHouse SDK to ensure
+              secure and reliable transactions, and deploy them in FVM. As an
+              added benefit, the original creator will receive 1% of royalties
+              on every purchase.
+            </p>
+            <Button
+              style={{ marginTop: "15px" }}
+              variant="outlined"
+              component="a"
+              href={"/create"}
+            >
+              Create your NFT
+            </Button>
+          </div>
+          <br />
+          <br />
+          <div style={{ textAlign: "center" }}>
+            <h4 style={{ color: "hsl(215, 51%, 70%)" }}>
+              Enjoy Exclusive Perks with Special Edition NFTs
+            </h4>
+            <p style={{ marginTop: "15px" }}>
+              {" "}
+              - Experience added benefits when purchasing Special Edition NFTs
+            </p>
+            <Button
+              style={{ marginTop: "15px" }}
+              variant="outlined"
+              component="a"
+              href="/all-nfts"
+            >
+              Buy NFT's
+            </Button>
+          </div>
+
+          <br />
+          <br />
+          <div style={{ textAlign: "center" }}>
+            <h4 style={{ color: "hsl(215, 51%, 70%)" }}>
+              Manage Your NFT Purchases with Ease
+            </h4>
+            <p style={{ marginTop: "15px" }}>
+              {" "}
+              - Keep track of your favorite creator's NFTs and your own
+              collection
+            </p>
+            <Button
+              style={{ marginTop: "15px" }}
+              variant="outlined"
+              component="a"
+              href="/my-purchases"
+            >
+              My Purchases
+            </Button>
+          </div>
+          <br />
+          <br />
+          <div style={{ textAlign: "center" }}>
+            <h4 style={{ color: "hsl(215, 51%, 70%)" }}>
+              Create Rooms and Talk to Your NFT Buyers
+            </h4>
+            <p style={{ marginTop: "15px" }}>
+              {" "}
+              - Engage with your NFT buyers in dedicated rooms powered by Huddle
+              SDK
+            </p>
+            <Button
+              style={{ marginTop: "15px" }}
+              variant="outlined"
+              component="a"
+              href="/all-rooms"
+            >
+              Create Room
+            </Button>
+            <Button
+              style={{ marginTop: "15px", marginLeft: "10px" }}
+              variant="outlined"
+              component="a"
+              href="/all-rooms"
+            >
+              All Active Rooms
+            </Button>
+          </div>
+          <br />
+          <br />
+          <div style={{ textAlign: "center" }}>
+            <h4 style={{ color: "hsl(215, 51%, 70%)" }}>
+              Connect with Creators through Special NFT Holder Chats
+            </h4>
+            <p style={{ marginTop: "15px" }}>
+              {" "}
+              - Foster direct communication using dedicated chat sections
+            </p>
+            <Button
+              style={{ marginTop: "15px" }}
+              variant="outlined"
+              component="a"
+              href="/all-special-edition-nft-buyers"
+            >
+              Chat with Creators
+            </Button>
           </div>
         </div>
-        <BottomNavigation
-          sx={{
-            width: 500,
-            margin: "auto",
-            position: "fixed",
-            bottom: 0,
-            left: 0,
-            right: 0,
-            backgroundColor: "hsl(216, 50%, 16%)",
-            // color: "",
-            zIndex: 999,
-          }}
-          value={value}
-          onChange={handleChange}
-        >
-          <BottomNavigationAction
-            label={isVideoOn ? "Video-On" : "Video-Off"}
-            value="mic-on"
-            onClick={handleVideoClick}
-            icon={
-              isVideoOn ? (
-                <VideocamIcon label="Video-On" value="video-on" />
-              ) : (
-                <VideocamOffIcon label="Video-Off" value="video-off" />
-              )
-            }
-          />
-          {/* <BottomNavigationAction
-            label="Mic-on"
-            value="mic-on"
-            icon={<MicIcon />}
-          /> */}
-          <BottomNavigationAction
-            label={isMicOn ? "Mic-On" : "Mic-Off"}
-            value="mic-on"
-            onClick={handleMicClick}
-            icon={
-              isMicOn ? (
-                <MicIcon label="Mic-On" value="mic-on" />
-              ) : (
-                <MicOffIcon label="Mic-Off" value="mic-off" />
-              )
-            }
-          />
-          <BottomNavigationAction
-            disabled={true}
-            label={isRecordingOn ? "Recording-On" : "Recording-Off"}
-            value="mic-on"
-            onClick={handleRecordClick}
-            icon={
-              isRecordingOn ? (
-                <RadioButtonCheckedIcon
-                  label="Recording-On"
-                  value="recording-on"
-                />
-              ) : (
-                <RadioButtonUncheckedIcon
-                  label="Recording-Off"
-                  value="recording-off"
-                />
-              )
-            }
-          />
-          {/* <BottomNavigationAction
-            label="Folder"
-            value="folder"
-            icon={<FolderIcon />}
-          /> */}
-        </BottomNavigation>
       </div>
     </>
   );
